8b2e1c13f5378876c5d4d3a973d002c313b94aa0
salvatore monggiotti tassinari <l18350507@gmail.com>
2022-05-13 09:02:50 -0500

    codigo terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,179 +1,169 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function App(){
-//TODO presentar el concepto de "state"
-// hrs
+function App() {
 
-const [inputState,setInputState] = useState({
-  titulo: "",
-  fecha: "",
-  nota: "",
-});
+//hooks : Funciones especificas de react todos comienzan con la palabra "use"
 
-const initialState= JSON.parse (localStorage.getItem("notas"))||[];
-const handleInputChange = (event) => {
-  // console.log(event.target);
-  setInputState({
-    ...inputState,
-  [event.target.name]: event.target.value,
-  });
-}; 
+  const [inputState,setInputState] = useState  ({
+    titulo: "",
+    fecha: "",
+    nota: "",
+  }); //VALOR INICIAL DEL STATE
 
-const handleClicklimpiar= () => {
-  setInputState({titulo:"",fecha:"",nota:""});
-};
+  const initialState = JSON.parse (localStorage.getItem ("notas")) || [];
+  const [notas, setNotas] = useState (initialState);
 
-const handleClickGuardar = () => {
-  arregloNotas.push(inputState);
-  localStorage.setItem("notas", JSON.stringify(arregloNotas));
-  handleClicklimpiar(); 
-};
+  const handleInputChange = (event) => { 
+      setInputState({
+        ...inputState,
+        [event.target.name]: event.target.value,
+    });
+  };
 
-const handleBorrarNota = (index) => {
-  const nuevoArreglo =[];
-  notas.forEach((nota,index)) => {
-    if (index i){
-      nuevoArreglo.push(nota);
+  const handleInputClean = ( ) => {
+    setInputState ({
+      titulo: " ", 
+      fecha: " ", 
+      nota: " "
+    });
+  };
 
-    }
-  };);
-};
+  const handleClickGuardar = ( ) => {
+    setNotas([...notas, inputState])
+    localStorage.setItem ("notas",JSON.stringify (notas));
+    handleInputClean();
+  };
 
+  const handleBorrarNota = (index) => {
+    const nuevoArreglo = []
+    
+    notas.forEach ((nota, i) => {
+      if (index !== i) {
+        nuevoArreglo.push (nota)
+      }
+    });
 
-const handleClicklimpiar =() => {
-  setNotas([]);
-  localStorage.setItem("notas",JSON.stringify([]));
-};
+    localStorage.setItem ("notas", JSON.stringify (nuevoArreglo))
+    setNotas ([...nuevoArreglo])
+  };
 
-return(
-  <div className="App container">
-    <div className="row">
-      <div className="col">
-        <h3>lista</h3>
-      </div>
+  const handleClickLimpiaLista = () => {
+    setNotas ([])
+      localStorage.setItem ("notas", JSON.stringify ([]));
+  };
+    
+  return (
+
+    <div className="App container">
+      <div className="row">
+        <div className= "col">
+          <h3> LISTAS </h3>
+          {notas.length === 0 && 
+            "Al momento no tienes notas guardadas. Puedes crear una en el formulario contiguo"}
+          
+          {notas.length !== 0 && (
+            <ol>
+              {notas.map((item, index) => {
+                return (
+                  <li key = {index}>
+                    {item.titulo}({item.fecha}) &nbsp;
+                    <i 
+                      className = "bi bi-x-circle-fill"
+                      onClick = {() => handleBorrarNota (index)}
+                      style = {{
+                        color: "red",
+                        fontSize: "0.75rem",
+                        cursor: "pointer",
+                      }} 
+                      ></i>
+                  </li>
+                );
+              })}
+            </ol>
+          )}
+          <button 
+            type = "button"
+            className = "btn btn-primary"
+            onClick = {handleClickLimpiaLista}
+            disabled={notas.length === 0}
+            > 
+            Limpia lista
+            </button>
+        </div>
         <div className="col">
-        <h3>Notas</h3>
-        <label className="mb-2" style={{ width: "100%"}}>
-          titulo
-          <input 
-          id = "titulo"
-          name="titulo"
-          type="text" 
-          onChange={handleInputChange}
-          value={inputState.titulo}
-          style={{ width: "100%"}}
-          />
-        </label>
-        <br />
-          <label className="mb-2" style={{ width: "100%"}}>
-          fecha
-          <input 
-          id = "fecha"
-          name="fecha"
-          type="text" 
+        <h3> NOTAS </h3>
+        <label className = 'mb-2' style={{width:"100%"}} > 
+
+        TITULO 
+
+        <input
+          id = "titulo" 
+          name = "titulo" 
+          type = "text"
+          onChange = {handleInputChange}
+          value = {inputState.titulo}
+          style = {{ width: "100%"}}
+        />
+      </label>
+      <br />
+
+    <label className = "mb-2"
+    style = {{ width: "100%"}}>
+      FECHA
+        <input 
+          id ="fecha" 
+          name="fecha" 
+          type="date"
           onChange={handleInputChange}
           value={inputState.fecha}
-          style={{ width: "100%"}}
+          style = {{ width: "100%"}}
           />
-          </label>
-          <br />
-          <label className="mb-2" style={{ width: "100%"}}>
-          nota
-          <input 
-          id = "nota"
-          name="nota"
-          type="text" 
+    </label>
+    <br />
+
+    <label className = "mb-2" 
+    style = {{ width: "100%"}}>
+      NOTA
+        <textarea 
+          id ="nota" 
+          name="nota" 
           onChange={handleInputChange}
           value={inputState.nota}
-          style={{ width: "100%"}}
+          style = {{ width: "100%"}}
           />
-        </label>
+    </label>
+    <hr />
+      <div className = "mt-2 me-2 mt-2 row">
+        <div className = "col">
+        <span className="row me-1">
+          <button 
+            type = "button" 
+            className ="btn btn-primary"
+            onClick={handleInputClean}
+        >
+            LIMPIAR
+          </button>
+        </span>
+      </div>
+      <div className = "col">
+        <span className = "row ms-1">
+          <button 
+            type="button" 
+            className="btn btn primary"
+            onClick={handleClickGuardar}
+            disabled={
+            inputState.titulo==="" ||
+            inputState.fecha ==="" ||
+            inputState.nota ===""}
+          >
+          GUARDAR
+              </button>
+              </span>
+            </div>   
+          </div>
         </div>
-
-    </div>
-
-  </div>
-);
-
-
-
-
-
-
-
-  return (
- <div className="App container">
-  <div className="row">
-  <div className="col">
-  <h3>lista</h3>
-  </div>
-  <div className="col">
-  <h3>Notas</h3>//</div>/
-  <label className="mb-2" style={{width: "100%"}}>
-    
-    <input
-    id="titulo"
-    name="titulo"
-    type="text"
-    onChange={handleInputChange}
-    value={inputState.titulo}
-    style={{width:"100%"}}
-    />
-    </label>
-    <br />
-    <label className="mb-2" style={{width:"100%"}}
-    fecha
-    <input
-    id="fecha"
-    name="fecha"
-    type="text"
-    onChange={handleInputChange}
-    value={inputState.fecha}
-    style={{width:"100%"}}
-    />
-    </label>
-    <br />
-    <label style={{width: "100%"}}>
-      Nota
-      <input
-      id="notas"
-      name="nota"
-      type="text"
-      onChange={handleInputChange}
-      value={inputState.nota}
-      style={{width:"100%"}}
-      onChange={handleInputChange}
-      value={inputState.nota}
-      style={{width:"100%"}}
-      />
-      </label>
-      <hr />
-      <div className="m-2 me-2 mt-2 row"> 
-        <div className="col">
-          <span className="row me-1">
-            <button
-            type="button"
-            className="btn btn-primary"
-            onClick={handleClicklimpiar}
-            > 
-            limpiar
-            </button>
-            </span>
-            </div>
-            <div className="col"> 
-              <span className="row ms-1">
-                <button
-                type="button"
-                className="btn btn-primary"
-                onClick={handleClickGuardar}
-                </div>
-                <div className=""
-
-
-
-
-  );
-}
-
-
-export default App;
+      </div>
+    </div>     
+    );
+  }
+  export default App;
